075abf3c9674a6d1334f329e8b92a28a08402446
Keith <ko.keith@hotmail.com>
2020-09-15 11:36:24 -0400

    finished project

--- src/AlgorithmVisualizer/AlgorithmVisualizer.jsx
+++ src/AlgorithmVisualizer/AlgorithmVisualizer.jsx
@@ -3,68 +3,144 @@ import Node from "./Node/Node";
 import "./AlgorithmVisualizer.css";
 import { dijkstra, getNodesInShortestPathOrder } from "../algorithms/dijkstra";
 
+const START_NODE_ROW = 10;
+const START_NODE_COL = 15;
+const FINISH_NODE_ROW = 10;
+const FINISH_NODE_COL = 35;
+
 export default class AlgorithmVisualizer extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      nodes: [],
+      grid: [],
+      mouseIsPressed: false,
     };
   }
 
   componentDidMount() {
-    //componentDidMount() is invoked immediately after a component is mounted (inserted into the tree)
+    const grid = getInitialGrid();
+    this.setState({ grid });
+  }
 
-    //create initial node array for 2D array
-    const nodes = [];
+  handleMouseDown(row, col) {
+    const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
+    this.setState({ grid: newGrid, mouseIsPressed: true });
+  }
 
-    //nested for loop to create 2D
-    for (let row = 0; row < 15; row++) {
-      const currentRow = [];
-      for (let col = 0; col < 40; col++) {
-        const currentNode = {
-          col,
-          row,
-          isStart: row === 10 && col === 5, //isStart only true if row == 10 and col == 5
-          isFinish: row === 10 && col === 35, //isFinish only true if row == 10 and col == 35
-        };
-        currentRow.push(currentNode); //create 50 array elements inside currentRow array
+  handleMouseEnter(row, col) {
+    if (!this.state.mouseIsPressed) return;
+    const newGrid = getNewGridWithWallToggled(this.state.grid, row, col);
+    this.setState({ grid: newGrid });
+  }
+
+  handleMouseUp() {
+    this.setState({ mouseIsPressed: false });
+  }
+
+  animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder) {
+    for (let i = 0; i <= visitedNodesInOrder.length; i++) {
+      if (i === visitedNodesInOrder.length) {
+        setTimeout(() => {
+          this.animateShortestPath(nodesInShortestPathOrder);
+        }, 10 * i);
+        return;
       }
-      nodes.push(currentRow);
-
-      //push row array into nodes array to build 2D array
-      //setState() enqueues changes to the component state and tells React that
-      //this component and its children need to be re-rendered with the updated state.
+      setTimeout(() => {
+        const node = visitedNodesInOrder[i];
+        document.getElementById(`node-${node.row}-${node.col}`).className =
+          "node node-visited";
+      }, 10 * i);
     }
-    this.setState({ nodes });
+  }
+
+  animateShortestPath(nodesInShortestPathOrder) {
+    for (let i = 0; i < nodesInShortestPathOrder.length; i++) {
+      setTimeout(() => {
+        const node = nodesInShortestPathOrder[i];
+        document.getElementById(`node-${node.row}-${node.col}`).className =
+          "node node-shortest-path";
+      }, 50 * i);
+    }
+  }
+
+  visualizeDijkstra() {
+    const { grid } = this.state;
+    const startNode = grid[START_NODE_ROW][START_NODE_COL];
+    const finishNode = grid[FINISH_NODE_ROW][FINISH_NODE_COL];
+    const visitedNodesInOrder = dijkstra(grid, startNode, finishNode);
+    const nodesInShortestPathOrder = getNodesInShortestPathOrder(finishNode);
+    this.animateDijkstra(visitedNodesInOrder, nodesInShortestPathOrder);
   }
 
   render() {
-    //set local variable nodes to global variable nodes
-    const { nodes } = this.state;
-    console.log(nodes);
+    const { grid, mouseIsPressed } = this.state;
 
     return (
-      <div className="grid">
-        {nodes.map((row, rowIdx) => {
-          //.map() iterates through array and does something to each element
-          return (
-            <div key={rowIdx}>
-              {row.map((node, nodeIdx) => {
-                const { isStart, isFinish } = node;
-                return (
-                  <Node
-                    key={nodeIdx}
-                    isStart={isStart}
-                    isFinish={isFinish}
-                    test={"foo"}
-                    test={"bar"}
-                  ></Node>
-                );
-              })}
-            </div>
-          );
-        })}
-      </div>
+      <>
+        <button onClick={() => this.visualizeDijkstra()}>
+          Visualize Dijkstra's Algorithm
+        </button>
+        <div className="grid">
+          {grid.map((row, rowIdx) => {
+            return (
+              <div key={rowIdx}>
+                {row.map((node, nodeIdx) => {
+                  const { row, col, isFinish, isStart, isWall } = node;
+                  return (
+                    <Node
+                      key={nodeIdx}
+                      col={col}
+                      isFinish={isFinish}
+                      isStart={isStart}
+                      isWall={isWall}
+                      mouseIsPressed={mouseIsPressed}
+                      onMouseDown={(row, col) => this.handleMouseDown(row, col)}
+                      onMouseEnter={(row, col) =>
+                        this.handleMouseEnter(row, col)
+                      }
+                      onMouseUp={() => this.handleMouseUp()}
+                      row={row}
+                    ></Node>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </>
     );
   }
 }
+const getInitialGrid = () => {
+  const grid = [];
+  for (let row = 0; row < 20; row++) {
+    const currentRow = [];
+    for (let col = 0; col < 50; col++) {
+      currentRow.push(createNode(col, row));
+    }
+    grid.push(currentRow);
+  }
+  return grid;
+};
+const createNode = (col, row) => {
+  return {
+    col,
+    row,
+    isStart: row === START_NODE_ROW && col === START_NODE_COL,
+    isFinish: row === FINISH_NODE_ROW && col === FINISH_NODE_COL,
+    distance: Infinity,
+    isVisited: false,
+    isWall: false,
+    previousNode: null,
+  };
+};
+const getNewGridWithWallToggled = (grid, row, col) => {
+  const newGrid = grid.slice();
+  const node = newGrid[row][col];
+  const newNode = {
+    ...node,
+    isWall: !node.isWall,
+  };
+  newGrid[row][col] = newNode;
+  return newGrid;
+};
